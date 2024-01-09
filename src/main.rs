5b68cda23e429ad55111e7d5e7c1279b07325b29
Johannes <65119813+Johannes29@users.noreply.github.com>
2024-01-09 11:28:23 +0100

    Revert "Add code to generate a build error"
    This reverts commit 5c406a8f64b283e48b168ab915380599d04a72d9.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn this_will_generate_a_warning(){
-    abc
-}
+fn this_will_generate_a_warning(){}
 
 // +x is to the right, +y is up, +z is forwards
 fn main() {
